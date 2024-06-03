52d47dce217249e8c470081da988501b09f1323e
hexzoner <hexzon@gmail.com>
2024-06-03 15:44:30 +0200

    finished first version of pig-latin

--- piglatin.js
+++ piglatin.js
@@ -1,7 +1,16 @@
 const args = process.argv.slice(2);
 // const argsLowered = args.map((x) => x.toLowerCase());
 // const input = argsLowered[0];
-console.log(args);
+// console.log(args);
+
+const fColor = {
+  reset: "\x1b[0m",
+  red: "\x1b[31m",
+  green: "\x1b[32m",
+  white: "\x1b[37m",
+  yellow: "\x1b[33m",
+  magenta: "\x1b[35m",
+};
 
 const consonats = "bcdfghjklmnpqrstvwxyz";
 const vowels = "aeiou";
@@ -11,20 +20,29 @@ if (args.length == 0) {
   return;
 }
 
-console.log("Running...");
+console.log(fColor.yellow + "User entered " + args.length + " words", fColor.white);
 
-for (let word of args) {
-  //   console.log(vowels.find((letter) => letter === word[0]));
-  //   console.log(vowels.some((letter) => letter === word[0]));
-  if (vowels.indexOf(word[0].toLowerCase()) != -1) {
-    console.log("We found a word starting with a vowel: " + word);
+for (let i = 0; i < args.length; i++) {
+  if (vowels.indexOf(args[i][0].toLowerCase()) != -1) {
+    // console.log("We found a word starting with a vowel: " + args[i] + " (index " + i + ")");
+    args[i] += "way";
   }
 
-  if (word.length == 1) continue;
+  if (args[i].length == 1) continue;
 
-  if (consonats.indexOf(word[0].toLowerCase()) != -1 && consonats.indexOf(word[1].toLowerCase() != -1)) {
-    console.log(word[0].toLowerCase());
-    console.log(word[1].toLowerCase());
-    console.log("We found a word starting with 2 consonants: " + word);
+  if (consonats.indexOf(args[i][0].toLowerCase()) !== -1 && consonats.indexOf(args[i][1].toLowerCase()) !== -1) {
+    console.log("We found a word starting with 2 consonants: " + args[i] + " (index " + i + ")");
+    let newWord = args[i].slice(2, args[i].length) + args[i][0].toLowerCase() + args[i][1] + "ay";
+    args[i] = newWord;
+  }
+
+  if (consonats.indexOf(args[i][0].toLowerCase()) !== -1 && vowels.indexOf(args[i][1].toLowerCase()) !== -1) {
+    // console.log("We found a word starting with consonat + vowel: " + args[i] + " (index " + i + ")");
+
+    let newWord = args[i].slice(1, args[i].length) + args[i][0].toLowerCase() + "ay";
+    // const upperCased = newWord[0].toUpperCase();
+    args[i] = newWord;
   }
 }
+
+console.log(fColor.red, args.join(" "));
