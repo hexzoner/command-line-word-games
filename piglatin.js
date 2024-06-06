1338e55b2590e6148aacac643e21f914f298f333
hexzoner <hexzon@gmail.com>
2024-06-06 17:26:21 +0200

    fixed piglatin and removing special characters from input

--- piglatin.js
+++ piglatin.js
@@ -15,44 +15,52 @@ const fColor = {
 
 const consonats = "bcdfghjklmnpqrstvwxyz";
 const vowels = "aeiou";
-const specials = "/`!@#$%^&*()_+-=[]{};':|,.<>?~]";
 
 if (args.length == 0) {
   console.log("No message was entered. Please enter your message and try again.");
   return;
 }
 
-console.log(fColor.yellow + "User entered " + args.length + " words", fColor.white);
+const inputText = args[0];
 
-for (let i = 0; i < args.length; i++) {
-  if (args[i].length == 1) continue;
+// const inputWords = inputText.split(" ");
+const inputWords = inputText.match(/\w+/g);
+//    / ... /g    It's a global regex. So it'll operate on multiple matches in the string.
+// \w    Matches any alphanumeric character from the basic Latin alphabet, including the underscore. Equivalent to [A-Za-z0-9_].
+
+console.log(inputWords);
+
+console.log(fColor.yellow + "User entered " + inputWords.length + " words", fColor.white);
+
+for (let i = 0; i < inputWords.length; i++) {
+  if (inputWords[i].length == 1) continue;
 
   let isUpperCase = false;
-  if (args[i][0] == args[i][0].toUpperCase()) isUpperCase = true;
+  if (inputWords[i][0] == inputWords[i][0].toUpperCase()) isUpperCase = true;
 
   //---special characters logic
   let hasDot = false;
-  if (args[i][args[i].length - 1] == ".") {
+  if (inputWords[i][inputWords[i].length - 1] == ".") {
     hasDot = true;
-    args[i] = args[i].substring(0, args[i].length - 1);
+    inputWords[i] = inputWords[i].substring(0, inputWords[i].length - 1);
   }
 
   //---a vowel---
-  if (vowels.indexOf(args[i][0].toLowerCase()) != -1) args[i] += "way";
+  if (vowels.indexOf(inputWords[i][0].toLowerCase()) != -1) inputWords[i] += "way";
 
   //---2 consonants---
-  if (consonats.indexOf(args[i][0].toLowerCase()) !== -1 && consonats.indexOf(args[i][1].toLowerCase()) !== -1) {
-    args[i] = args[i].slice(2, args[i].length) + args[i][0].toLowerCase() + args[i][1] + "ay";
+  if (consonats.indexOf(inputWords[i][0].toLowerCase()) !== -1 && consonats.indexOf(inputWords[i][1].toLowerCase()) !== -1) {
+    inputWords[i] = inputWords[i].slice(2, inputWords[i].length) + inputWords[i][0].toLowerCase() + inputWords[i][1] + "ay";
   }
 
   // -- a consonant and a vowel---
-  if (consonats.indexOf(args[i][0].toLowerCase()) !== -1 && vowels.indexOf(args[i][1].toLowerCase()) !== -1) {
-    args[i] = args[i].slice(1, args[i].length) + args[i][0].toLowerCase() + "ay";
+  if (consonats.indexOf(inputWords[i][0].toLowerCase()) !== -1 && vowels.indexOf(inputWords[i][1].toLowerCase()) !== -1) {
+    inputWords[i] = inputWords[i].slice(1, inputWords[i].length) + inputWords[i][0].toLowerCase() + "ay";
   }
 
   //---final fomatting logic
-  if (isUpperCase) args[i] = CapitalizeFirstLetter(args[i]);
-  if (hasDot) args[i] += ".";
+  if (isUpperCase) inputWords[i] = CapitalizeFirstLetter(inputWords[i]);
+  if (hasDot) inputWords[i] += ".";
 }
 
-console.log(fColor.red, args.join(" "));
+console.log(fColor.red, inputWords.join(" "));
